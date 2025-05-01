e50fe0f3c8099cc15e8e7fbf1358e177badd564f
Riosberg Dutra <riosbergduttra@gmail.com>
2025-05-01 13:07:42 -0300

    arquivos bases backend

---
+++ furia/src/main/java/com/kyf/furia/FuriaApplication.java
@@ -0,0 +1,13 @@
+package com.kyf.furia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuriaApplication.class, args);
+	}
+
+}
---
+++ furia/src/test/java/com/kyf/furia/FuriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyf.furia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
